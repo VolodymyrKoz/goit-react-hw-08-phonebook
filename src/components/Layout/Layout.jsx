23d2d3048c12da673388531c4df6bf096ed67edf
VolodymyK <114539981+VolodymyK@users.noreply.github.com>
2023-07-28 14:40:25 -0500

    done

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import { AppBar } from 'components/AppBar/AppBar';
+import { Suspense } from 'react'; // для лінивої загрузки
+import { Outlet } from 'react-router-dom';
+
+export const Layout = () => {
+  return (
+    <>
+      {' '}
+      {/* для того щоб AppBar не перекривав контент */}
+      <AppBar />
+      {/* для лінивої загрузки */}
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+    </>
+  );
+};
+
+// Діма Берестень
